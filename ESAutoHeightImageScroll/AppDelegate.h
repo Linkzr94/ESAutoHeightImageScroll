51c44b5f0a090d8457dcba25f3d9e15c5923297d
maominghiu <maominghui1993@gmail.com>
2018-08-31 16:41:07 +0800

    Initial Commit

---
+++ ESAutoHeightImageScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ESAutoHeightImageScroll
+//
+//  Created by MaoMinghui on 2018/8/31.
+//  Copyright © 2018年 lql. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
